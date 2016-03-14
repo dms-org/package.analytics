5a698cb4426465c378572c5db37bb4f1a1d5212f
Elliot Levin <elliot@iddigital.com.au>
2016-03-15 10:12:41 +1100

    Add more helpful error message

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -2,5 +2,5 @@
 <?php
 
 return [
-    'api-details-failure' => 'The supplied API credentials are invalid'
+    'api-details-failure' => 'The supplied API credentials are invalid, does the service account have access to the supplied analytics view?'
 ];
